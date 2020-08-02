dc5e0958e65b6bdb5d238e52fd3e25574e1feffe
Yasunari Watanabe <yasuwata27@gmail.com>
2020-08-03 01:02:18 +0800

    Add documentation

--- lib/core.v
+++ lib/core.v
@@ -5,14 +5,19 @@ Require Import prelude pred pcm unionmap heap.
 From HTT
 Require Import stmod stsep stlog stlogR.
 
-(* SuSLik bindings *)
+(* The empty heap *)
 Notation empty := Unit.
+
+(* Register the value 0 as null *)
 Coercion ptr_nat : nat >-> ptr.
+
+(* No-op at the end of a program branch *)
 Definition skip := ret tt.
 
 Inductive ltac_No_arg : Set :=
 | ltac_no_arg : ltac_No_arg.
 
+(* Utilities to move a heaplet around within a heap *)
 Ltac put_to_tail_ptr p :=
   rewrite -?joinA; rewrite ?(joinC (p :-> _) _); rewrite ?joinA.
 
@@ -23,16 +28,89 @@ Ltac put_to_head h :=
   put_to_tail h;
   rewrite (joinC _ h).
 
+(* Store heap validity assertions *)
+Ltac store_valid :=
+  rewrite ?unitL ?unitR;
+  try match goal with
+  | [|- is_true (valid _) -> _] =>
+    let hyp := fresh "H_valid" in move=>hyp;
+    store_valid
+  end
+.
+
+(* If goal is to prove a pointer is not null, derive that fact from one of the heap validity assertions *)
+Ltac assert_not_null :=
+  match goal with
+  | [H: is_true (valid ?h) |- is_true (?x != ptr_nat 0)] =>
+    rewrite ?joinA in H;
+    rewrite -?(joinC (x :-> _)) in H;
+    rewrite ?joinA in H;
+    move:(H);
+    rewrite defPtUnO;
+    case/andP;
+    let not_null := fresh "not_null" in move=>not_null _;
+    assumption
+  end.
+
+(* Unfold a constructor  *)
+Ltac unfold_constructor n :=
+  match goal with
+  (* There was nothing to solve; this happens if a preceding step *)
+  (* unwittingly solved the intended goal without needing to unfold. *)
+  | [|- verify _ _ _] =>
+    idtac
+  (* Unfold and discharge selector goal if needed *)
+  | _ => match n with
+         | 1 => constructor 1=>//
+         | 2 => constructor 2=>//
+         | 3 => constructor 3=>//
+         | _ => constructor=>//
+         end;
+         match goal with
+         | [|- is_true (_ != ptr_nat 0)] => assert_not_null
+         | _ => idtac
+         end
+  end.
+
+(* Reasoning about singleton subsets *)
+Lemma subset_singleton :
+    forall (s1: seq nat) (x: nat),
+      {subset s1 <= [::x]} -> {subset x :: s1 <= [::x]}.
+intros.
+move=>n.
+rewrite inE; move/orP. case.
+move/eqP=>->. by rewrite inE eqxx.
+move/H=>//.
+Qed.
+
+
+(***********)
+(* Tactics *)
+(***********)
+
+(* After binding program variables to their correct labels, we use the Coq's default simplification algorithm *)
+Ltac ssl_program_simpl := Tactics.program_simpl.
+
+
+(* Ghost Variable Elim *)
+Ltac ssl_ghostelim_pre := try apply: ghR; move=>h.
+
+Ltac ssl_ghostelim_post := store_valid.
+
+(* Read Rule *)
 Ltac ssl_read := apply: bnd_readR=>/=.
 
+(* Write Rule *)
 Ltac ssl_write := apply: bnd_writeR=>/=.
 
 Ltac ssl_write_post x :=
   (put_to_tail_ptr x + rewrite -(unitL (x :-> _))); apply frame.
 
+(* Alloc Rule *)
 Ltac ssl_alloc x :=
   apply: bnd_allocbR=>x//=.
 
+(* Free Rule *)
 Ltac ssl_dealloc :=
   apply: bnd_seq;
   match goal with
@@ -47,28 +125,8 @@ Ltac ssl_dealloc :=
   end
 .
 
-Ltac assert_not_null :=
-  match goal with
-  | [H: is_true (valid ?h) |- is_true (?x != ptr_nat 0)] =>
-    rewrite ?joinA in H;
-    rewrite -?(joinC (x :-> _)) in H;
-    rewrite ?joinA in H;
-    move:(H);
-    rewrite defPtUnO;
-    case/andP;
-    let not_null := fresh "not_null" in move=>not_null _;
-    assumption
-  end.
 
-Ltac store_valid :=
-  rewrite ?unitL ?unitR;
-  try match goal with
-  | [|- is_true (valid _) -> _] =>
-    let hyp := fresh "H_valid" in move=>hyp;
-    store_valid
-  end
-.
-
+(* Call Rule *)
 Ltac ssl_call_pre_aux h :=
   match h with
   | ?h1 \+ ?h2 => ssl_call_pre_aux h2; put_to_head h1
@@ -92,19 +150,11 @@ Ltac ssl_call' ex :=
 Tactic Notation "ssl_call" := ssl_call' ltac_No_arg.
 Tactic Notation "ssl_call" constr(ex) := ssl_call' ex.
 
+(* Emp Rule *)
 Ltac ssl_emp := apply: val_ret; rewrite ?unitL; store_valid; move=>//.
 
-Lemma subset_singleton :
-    forall (s1: seq nat) (x: nat),
-      {subset s1 <= [::x]} -> {subset x :: s1 <= [::x]}.
-intros.
-move=>n.
-rewrite inE; move/orP. case.
-move/eqP=>->. by rewrite inE eqxx.
-move/H=>//.
-Qed.
-
 Ltac ssl_emp_post :=
+  (* Various strategies to solve all parts of the current goal except the predicate applications  *)
   match goal with
   | [|- verify _ _ _] => idtac
   | _ => rewrite ?unitL ?unitR;
@@ -122,25 +172,7 @@ Ltac ssl_emp_post :=
                 end
   end.
 
-Ltac unfold_constructor n :=
-  match goal with
-  | [|- verify _ _ _] => idtac
-  | _ => match n with
-         | 1 => constructor 1=>//
-         | 2 => constructor 2=>//
-         | 3 => constructor 3=>//
-         | _ => constructor=>//
-         end;
-         match goal with
-         | [|- is_true (_ != ptr_nat 0)] => assert_not_null
-         | _ => idtac
-         end
-  end.
-
-Ltac ssl_ghostelim_pre := try apply: ghR; move=>h.
-
-Ltac ssl_ghostelim_post := store_valid.
-
+(* Open Rule *)
 Ltac ssl_open := case: ifP=> H_cond.
 Ltac ssl_open_post H :=
   case H;
@@ -150,4 +182,5 @@ Ltac ssl_open_post H :=
   end;
   move=>_.
 
-Ltac ssl_program_simpl := Tactics.program_simpl.
+(* Abduce Branch Rule *)
+Ltac ssl_abduce_branch := case: ifP=>H_cond.
