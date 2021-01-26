08e1484c926da55c86b7f7c3d27c91a24221b7af
Yasunari Watanabe <yasuwata27@gmail.com>
2021-01-26 17:43:46 +0800

    Add more benchmarks

---
+++ examples/sll_max.v
@@ -0,0 +1,112 @@
+From mathcomp
+Require Import ssreflect ssrbool ssrnat eqtype seq ssrfun.
+From fcsl
+Require Import prelude pred pcm unionmap heap.
+From HTT
+Require Import stmod stsep stlog stlogR.
+From SSL
+Require Import core.
+
+Inductive sll (x : ptr) (len : nat) (lo : nat) (hi : nat) (h : heap) : Prop :=
+| sll_1 of x == null of
+  hi == 0 /\ len == 0 /\ lo == 7 /\ h = empty
+| sll_2 of (x == null) = false of
+  exists (len1 : nat) (v : nat) (hi1 : nat) (lo1 : nat) (nxt : ptr),
+  exists h_sll_nxtlen1lo1hi1_513,
+  0 <= len1 /\ 0 <= v /\ hi == (if hi1 <= v then v else hi1) /\ len == 1 + len1 /\ lo == (if v <= lo1 then v else lo1) /\ v <= 7 /\ h = x :-> v \+ x .+ 1 :-> nxt \+ h_sll_nxtlen1lo1hi1_513 /\ sll nxt len1 lo1 hi1 h_sll_nxtlen1lo1hi1_513.
+
+Lemma pure1 : 0 == 0. Admitted.
+Hint Resolve pure1: ssl_pure.
+Lemma pure2 : 7 == 7. Admitted.
+Hint Resolve pure2: ssl_pure.
+Lemma pure3 len1x : 0 <= len1x -> 1 + len1x == 1 + len1x. Admitted.
+Hint Resolve pure3: ssl_pure.
+Lemma pure4 hi1x2 vx2 : vx2 <= 7 -> 0 <= vx2 -> (if hi1x2 <= vx2 then vx2 else hi1x2) == (if hi1x2 <= vx2 then vx2 else hi1x2). Admitted.
+Hint Resolve pure4: ssl_pure.
+Lemma pure5 vx2 lo1x : vx2 <= 7 -> 0 <= vx2 -> (if vx2 <= lo1x then vx2 else lo1x) == (if vx2 <= lo1x then vx2 else lo1x). Admitted.
+Hint Resolve pure5: ssl_pure.
+
+Definition sll_max_type :=
+  forall (vprogs : ptr * ptr),
+  {(vghosts : nat * nat * nat * ptr)},
+  STsep (
+    fun h =>
+      let: (x, r) := vprogs in
+      let: (n, lo, hi, a) := vghosts in
+      exists h_sll_xnlohi_514,
+      h = r :-> a \+ h_sll_xnlohi_514 /\ sll x n lo hi h_sll_xnlohi_514,
+    [vfun (_: unit) h =>
+      let: (x, r) := vprogs in
+      let: (n, lo, hi, a) := vghosts in
+      exists h_sll_xnlohi_515,
+      h = r :-> hi \+ h_sll_xnlohi_515 /\ sll x n lo hi h_sll_xnlohi_515
+    ]).
+
+Program Definition sll_max : sll_max_type :=
+  Fix (fun (sll_max : sll_max_type) vprogs =>
+    let: (x, r) := vprogs in
+    Do (
+      if x == null
+      then
+        r ::= 0;;
+        ret tt
+      else
+        vx2 <-- @read nat x;
+        nxtx2 <-- @read ptr (x .+ 1);
+        sll_max (nxtx2, r);;
+        hi1x2 <-- @read nat r;
+        r ::= (if hi1x2 <= vx2 then vx2 else hi1x2);;
+        ret tt
+    )).
+Obligation Tactic := intro; move=>[x r]; ssl_program_simpl.
+Next Obligation.
+ssl_ghostelim_pre.
+move=>[[[n lo] hi] a2].
+ex_elim h_sll_xnlohi_514.
+move=>[sigma_self].
+subst h_self.
+move=>H_sll_xnlohi_514.
+ssl_ghostelim_post.
+ssl_open (x == null);
+ssl_open_post H_sll_xnlohi_514.
+move=>[phi_sll_xnlohi_5140] [phi_sll_xnlohi_5141] [phi_sll_xnlohi_5142].
+move=>[sigma_sll_xnlohi_514].
+subst h_sll_xnlohi_514.
+ssl_write r.
+ssl_write_post r.
+ssl_emp;
+exists (empty);
+sslauto;
+solve [
+unfold_constructor 1;
+sslauto ].
+ex_elim len1x vx2 hi1x2 lo1x nxtx2.
+ex_elim h_sll_nxtx2len1xlo1xhi1x2_513x.
+move=>[phi_sll_xnlohi_5140] [phi_sll_xnlohi_5141] [phi_sll_xnlohi_5142] [phi_sll_xnlohi_5143] [phi_sll_xnlohi_5144] [phi_sll_xnlohi_5145].
+move=>[sigma_sll_xnlohi_514].
+subst h_sll_xnlohi_514.
+move=>H_sll_nxtx2len1xlo1xhi1x2_513x.
+ssl_read x.
+ssl_read (x .+ 1).
+ssl_call_pre (r :-> a2 \+ h_sll_nxtx2len1xlo1xhi1x2_513x).
+ssl_call (len1x, lo1x, hi1x2, a2).
+exists (h_sll_nxtx2len1xlo1xhi1x2_513x);
+sslauto.
+move=>h_call1.
+ex_elim h_sll_nxtx2len1xlo1xhi1x2_5151.
+move=>[sigma_call1].
+subst h_call1.
+move=>H_sll_nxtx2len1xlo1xhi1x2_5151.
+store_valid.
+ssl_read r.
+ssl_write r.
+ssl_write_post r.
+ssl_emp;
+exists (x :-> vx2 \+ x .+ 1 :-> nxtx2 \+ h_sll_nxtx2len1xlo1xhi1x2_5151);
+sslauto;
+solve [
+unfold_constructor 2;
+exists (len1x), (vx2), (hi1x2), (lo1x), (nxtx2);
+exists (h_sll_nxtx2len1xlo1xhi1x2_5151);
+sslauto ].
+Qed.
---
+++ examples/srtl_prepend.v
@@ -0,0 +1,84 @@
+From mathcomp
+Require Import ssreflect ssrbool ssrnat eqtype seq ssrfun.
+From fcsl
+Require Import prelude pred pcm unionmap heap.
+From HTT
+Require Import stmod stsep stlog stlogR.
+From SSL
+Require Import core.
+
+Inductive sll (x : ptr) (len : nat) (lo : nat) (hi : nat) (h : heap) : Prop :=
+| sll_1 of x == null of
+  hi == 0 /\ len == 0 /\ lo == 7 /\ h = empty
+| sll_2 of (x == null) = false of
+  exists (len1 : nat) (v : nat) (hi1 : nat) (lo1 : nat) (nxt : ptr),
+  exists h_sll_nxtlen1lo1hi1_513,
+  0 <= len1 /\ 0 <= v /\ hi == (if hi1 <= v then v else hi1) /\ len == 1 + len1 /\ lo == (if v <= lo1 then v else lo1) /\ v <= 7 /\ h = x :-> v \+ x .+ 1 :-> nxt \+ h_sll_nxtlen1lo1hi1_513 /\ sll nxt len1 lo1 hi1 h_sll_nxtlen1lo1hi1_513.
+
+Inductive srtl (x : ptr) (len : nat) (lo : nat) (hi : nat) (h : heap) : Prop :=
+| srtl_1 of x == null of
+  hi == 0 /\ len == 0 /\ lo == 7 /\ h = empty
+| srtl_2 of (x == null) = false of
+  exists (len1 : nat) (v : nat) (hi1 : nat) (lo1 : nat) (nxt : ptr),
+  exists h_srtl_nxtlen1lo1hi1_514,
+  0 <= len1 /\ 0 <= v /\ hi == (if hi1 <= v then v else hi1) /\ len == 1 + len1 /\ lo == (if v <= lo1 then v else lo1) /\ v <= 7 /\ v <= lo1 /\ h = x :-> v \+ x .+ 1 :-> nxt \+ h_srtl_nxtlen1lo1hi1_514 /\ srtl nxt len1 lo1 hi1 h_srtl_nxtlen1lo1hi1_514.
+
+Lemma pure1 n : 0 <= n -> n + 1 == 1 + n. Admitted.
+Hint Resolve pure1: ssl_pure.
+Lemma pure2 k lo : k <= lo -> k <= 7 -> 0 <= k -> k == (if k <= lo then k else lo). Admitted.
+Hint Resolve pure2: ssl_pure.
+
+Definition srtl_prepend_type :=
+  forall (vprogs : ptr * nat * ptr),
+  {(vghosts : nat * nat * nat * ptr)},
+  STsep (
+    fun h =>
+      let: (x, k, r) := vprogs in
+      let: (n, lo, hi, a) := vghosts in
+      exists h_srtl_xnlohi_515,
+      0 <= k /\ 0 <= n /\ k <= 7 /\ k <= lo /\ h = r :-> a \+ h_srtl_xnlohi_515 /\ srtl x n lo hi h_srtl_xnlohi_515,
+    [vfun (_: unit) h =>
+      let: (x, k, r) := vprogs in
+      let: (n, lo, hi, a) := vghosts in
+      exists n1 y hi1,
+      exists h_srtl_yn1khi1_516,
+      n1 == n + 1 /\ h = r :-> y \+ h_srtl_yn1khi1_516 /\ srtl y n1 k hi1 h_srtl_yn1khi1_516
+    ]).
+
+Program Definition srtl_prepend : srtl_prepend_type :=
+  Fix (fun (srtl_prepend : srtl_prepend_type) vprogs =>
+    let: (x, k, r) := vprogs in
+    Do (
+      y2 <-- allocb null 2;
+      r ::= y2;;
+      (y2 .+ 1) ::= x;;
+      y2 ::= k;;
+      ret tt
+    )).
+Obligation Tactic := intro; move=>[[x k] r]; ssl_program_simpl.
+Next Obligation.
+ssl_ghostelim_pre.
+move=>[[[n lo] hi] a2].
+ex_elim h_srtl_xnlohi_515.
+move=>[phi_self0] [phi_self1] [phi_self2] [phi_self3].
+move=>[sigma_self].
+subst h_self.
+move=>H_srtl_xnlohi_515.
+ssl_ghostelim_post.
+ssl_alloc y2.
+ssl_write r.
+ssl_write_post r.
+ssl_write (y2 .+ 1).
+ssl_write_post (y2 .+ 1).
+ssl_write y2.
+ssl_write_post y2.
+ssl_emp;
+exists (n + 1), (y2), ((if hi <= k then k else hi));
+exists (y2 :-> k \+ y2 .+ 1 :-> x \+ h_srtl_xnlohi_515);
+sslauto;
+solve [
+unfold_constructor 2;
+exists (n), (k), (hi), (lo), (x);
+exists (h_srtl_xnlohi_515);
+sslauto ].
+Qed.
